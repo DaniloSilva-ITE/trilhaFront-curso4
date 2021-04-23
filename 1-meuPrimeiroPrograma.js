4a88ef8357b88b5ae923b71e10b30a0e42dfaf40
Danilo Silva <danilo.silva@itenabler.com.br>
2021-04-23 15:54:24 -0300

    Cap 1

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+console.log("Meu primeiro programa: Trabalhando com variaveis");
+
+const idade = 19;
+
+console.log("Idade:", idade);
+console.log("Idade+2:", idade+2);
+console.log("Idade-2:", idade-2);
+console.log("Idade*2:", idade*2);
+console.log("Idade/2:", idade/2);
+
+const idadeSomada = idade+2;
+console.log("IdadeSomada:",idadeSomada);
