eb241af1554136f2657ae192c1774a6414d2b047
Danilo Silva <danilo.silva@itenabler.com.br>
2021-04-26 11:46:27 -0300

    Cap 3

---
+++ 5-contador.js
@@ -0,0 +1,9 @@
+console.log("Continuacao do 'Trabalhando com atribuicao de variaveis'")
+
+let contador = 0;
+contador = contador + 1;
+
+let idade; // declarando variavel
+idade = 19 // atribuindo valor
+idade = idade + 1;
+console.log(idade);
---
+++ 6-listas.js
@@ -0,0 +1,44 @@
+console.log("Trabalhando com listas");
+
+const salvador = "Salvador";
+const saoPaulo = "São Paulo";
+const rioDeJaneiro = "Rio de Janeiro";
+
+console.log("Destinos Possiveis:")
+console.log(salvador, saoPaulo, rioDeJaneiro);
+
+// uma lista de variaveis
+const listaVarDeDestinos = new Array(
+    salvador, 
+    saoPaulo,
+    rioDeJaneiro
+);
+console.log("Destinos Possiveis (var):")
+console.log(listaVarDeDestinos);
+
+
+// um lista de strings
+const listaStringsDeDestinos = new Array(
+    "Salvador", 
+    "São Paulo",
+    "Rio de Janeiro"
+);
+console.log("Destinos Possiveis (str):")
+console.log(listaStringsDeDestinos);
+
+
+// add item a lista
+listaStringsDeDestinos.push("Curitiba");
+console.log(listaStringsDeDestinos);
+
+/* remove ultimo item da lista
+listaStringsDeDestinos.pop();
+console.log(listaStringsDeDestinos);*/
+
+// remove 1 item apartir da pos 2 da lista
+listaStringsDeDestinos.splice(2,1);
+console.log(listaStringsDeDestinos);
+
+// imprimi a pos correspondente
+console.log(listaStringsDeDestinos[1]);
+
